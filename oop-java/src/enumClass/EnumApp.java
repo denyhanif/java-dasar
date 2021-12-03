0c1488b02d5b3e274037c8d0d84158260586f7ac
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-12-03 22:40:45 +0700

    enum clas

---
+++ oop-java/src/enumClass/EnumApp.java
@@ -0,0 +1,12 @@
+package enumClass;
+
+public class EnumApp {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        customer.setName("deny");
+        customer.setLevel(Level.STANDARD);
+
+        System.out.println(customer.getName());
+        System.out.println(customer.getLevel());
+    }
+}
---
+++ oop-java/src/enumClass/Level.java
@@ -0,0 +1,9 @@
+package enumClass;
+
+public enum Level {
+    //best practice dengan hrn kapital
+
+    STANDARD,
+    PREMIUM,
+    VIP,
+}

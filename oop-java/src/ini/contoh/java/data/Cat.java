32d751f7782dcdc0b8f6cea5da53ff9d8a67f78e
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-12-03 14:25:13 +0700

    Abstact Method

---
+++ oop-java/src/ini/contoh/java/data/Cat.java
@@ -0,0 +1,10 @@
+package ini.contoh.java.data;
+
+public class Cata extends Animal {
+
+    //method run wajib di tulis karna merupakan abtrarc method dari parent
+    public void run(){
+        System.out.println("Cat "+name+" is RUNN");
+
+    }
+}

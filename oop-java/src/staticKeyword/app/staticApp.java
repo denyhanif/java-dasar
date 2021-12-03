8941d6d08057e32f41ca3494704565bab55bb349
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-12-03 22:10:19 +0700

    static keyword + static block, static import

--- oop-java/src/staticKeyword/app/staticApp.java
+++ oop-java/src/staticKeyword/app/staticApp.java
@@ -1,9 +1,10 @@
 package staticKeyword.app;
 import staticKeyword.data.*;
+import static staticKeyword.data.staticVariable.APPLICATION;
 public class staticApp {
     public static void main(String[] args) {
         //static var
-        System.out.println(staticVariable.APPLICATION);
+        System.out.println(APPLICATION);
         System.out.println(staticVariable.VERSION);
         //static func
         System.out.println(statisMethod.sum(1,2,2));

af546e66fbf11e3187f8a25108f9863fee42cd8f
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-12-07 20:25:15 +0700

    model dan view

--- todoo-basic/src/todoo.java
+++ todoo-basic/src/todoo.java
@@ -1,6 +1,51 @@
 public class todoo {
+    /**
+     * \model = variabel array utk menapung to do
+     */
+    public  static String[] model= new String[10];
     public static void main(String[] args) {
 
     }
-    
+
+    /**
+     * Menampilkan ToDoList
+     */
+    public static void showTodoList(){
+
+    }
+
+
+    /**
+     * Menambah data to do
+     */
+    public static void addTodoList(){
+
+    }
+
+    /**
+     * Hapus
+     */
+    public static void removeTodoList(){
+
+    }
+    /**
+     * View Tampilan toDo list
+     */
+
+    public static void viewShowTodoList(){
+
+    }
+    /**
+     * View Tambah toDo list
+     */
+    public static void viewAddTodoList(){
+
+    }
+    /**
+     * View Remove toDo list
+     */
+    public static void removeShowTodoList(){
+
+    }
+
 }

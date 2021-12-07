4a639793269cf9bae79f0d5112865f8ec7aa79eb
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-12-07 22:39:49 +0700

    add,show toido

--- todoo-basic/src/todoo.java
+++ todoo-basic/src/todoo.java
@@ -4,6 +4,8 @@ public class todoo {
      */
     public  static String[] model= new String[10];
     public static void main(String[] args) {
+        testAddTodoList();
+
 
     }
 
@@ -11,15 +13,64 @@ public class todoo {
      * Menampilkan ToDoList
      */
     public static void showTodoList(){
+        for (var i =0; i< model.length;i++){
+            var todo= model[i];
+            var no=i+1;
 
+            if(todo!=null){
+                System.out.println(no+ ". " +todo);
+            }
+        }
+
+    }
+    public static void testShowTodoList(){
+        model[0]="Belajar";
+        model[1]="todo list";
+        showTodoList();
     }
 
 
     /**
      * Menambah data to do
      */
-    public static void addTodoList(){
+    public static void addTodoList(String todo){
 
+        //pengecekan apakah data model sudah penuh?
+        var penuh= true;
+        for(int i =0;i< model.length;i++){
+            if (model[i]==null){
+                //model masih ada index yang kosong
+                penuh= false;
+                break;
+            }
+        }
+
+        //jika penuh, rezise aray jadi duakali lipat
+        if(penuh){
+            var data_sementara= model;// mnyimpan data model yang lama
+            model = new String[model.length*2];
+            //pindah data ke array yang sudah direzie
+            for (int i=0;i<data_sementara.length;i++){
+                model[i]=data_sementara[i];
+            }
+
+        }
+
+        //tambah data ke index yang null
+        for(var i=0; i< model.length;i++){
+            if(model[i]==null){
+                model[i] =todo;
+                break;
+            }
+        }
+
+    }
+    public static void testAddTodoList(){
+        for(int i =0;i<25;i++){
+            var nomer= i+1;
+            addTodoList("Testong Todo ke. "+nomer);
+        }
+        showTodoList();
     }
 
     /**
